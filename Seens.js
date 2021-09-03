dd8814bec4040d5bcefbb2d7a023e0eb1f40766d
raincoat05 <kaito_19n1100109@nnn.ed.jp>
2021-09-03 10:05:55 +0900

    kyo

--- Seens.js
+++ Seens.js
@@ -8,3 +8,31 @@ class Seen{
         this.sideCharacter = sideCharacter;
     };
 };
+
+var data= [];
+data.push(new Seen('hanako','iiiiii','th.jpeg'));
+data.push(new Seen('tarou','こんにちは','th2.jpeg'));
+data.push(new Seen('hanako','やあ','th.jpeg'));
+data.push(new Seen('tarou','こんにちは','th.jpeg'));
+
+var count = 0;
+
+var skip = function(){
+    document.getElementsByClassName("speaker")[0].innerHTML=data[count].name;
+    document.getElementsByClassName("speakText")[0].innerHTML=data[count].text;
+    document.getElementById("background").src='./img/background/'+ data[count].background;
+    document.getElementById("mainCharacter").src='./img/character/'+ data[count].name+ '.png';
+    console.log(data[count].name);
+    console.log(data[count].text);
+    count++;
+};
+
+$(function(){
+    $(skip);
+});
+
+$(function () {
+    $(".skipButton").click(function () {
+        $(skip);
+    });
+});
