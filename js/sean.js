b844de9b44f93d15070736adf97eadac7a611572
raincoat05 <kaito_19n1100109@nnn.ed.jp>
2021-09-06 09:56:07 +0900

    characterCodeChange

--- js/sean.js
+++ js/sean.js
@@ -1,17 +1,14 @@
 class Sean{
-    constructor(speakerName,speakText,background,character1,character2){
+    constructor(speakerName,speakText,background,character1){
         this.speakerName = speakerName;
         this.speakText = speakText;
         this.background = background;
         this.character1 = character1;
-        this.character2 = character2;
     };
 };
 
 var data= [];
-data.push(new Sean('hanako','iiiiii','th.jpeg','character1','character2'));
-data.push(new Sean('hanako','iiiiii','th.jpeg','character2','character2'));
-data.push(new Sean('hanako','iiiiii','th.jpeg','character1','character1'));
+data.push(new Sean('speaker','text','background','character'));
 
 var count = 0;
 
@@ -19,8 +16,7 @@ var skip = function(){
     document.getElementsByClassName("speakerName")[0].innerHTML=data[count].speakerName;
     document.getElementsByClassName("speakText")[0].innerHTML=data[count].speakText;
     document.getElementById("background").src='../img/background/'+ data[count].background;
-    document.getElementById("character1").src='../img/character/'+ data[count].character1+ '.png';
-    document.getElementById("character2").src='../img/character/'+ data[count].character2+ '.png';
+    document.getElementById("character").src='../img/character/'+ data[count].character+ '.png';
     count++;
 };
 
