c25f2c0ab44c0da34029a389dbd48ed6818e4382
raincoat05 <kaito_19n1100109@nnn.ed.jp>
2021-09-10 11:30:11 +0900

    question追加

---
+++ js/sean1.js
@@ -0,0 +1,134 @@
+class Sean{
+    constructor(speakerName,speakText,background,character1){
+        this.speakerName = speakerName;
+        this.speakText = speakText;
+        this.background = background;
+        this.character1 = character1;
+    };
+};
+
+var data= [];
+data.push(new Sean('探偵ブラン','おめでとう！！きみが最初の正かい者だ','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','ぜひともわたしの助手になってくれ！','sss.jpg','blanc'));
+data.push(new Sean('主人公','もちろんです！','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','ありがとう！早速だが説明させてもらおう','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','まず、きみにはこの予告状をみてもらいたい','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','見ての通りこの予告状は暗号になっている','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','この内ようがわからないとわたしたちは怪盗のはん行を止めることができない','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','そこで、試験を見事とっぱしたきみに、ぜひこれをといてもらいたい','sss.jpg','blanc'));
+data.push(new Sean('主人公','わかりました！がんばります！','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','ふむふむ......なるほど！きみのおかげで怪盗Noirの目的は、ルーブル美術館の絵画、「モナ・リザ」だと分かった！君のおかげだ','background','blanc'));
+data.push(new Sean('探偵ブラン','それでは早速現地に向かう','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','きみはそこからわたしをサポートしてくれ！','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','もしもし、私だ。たった今ルーブル美じゅつ館にとう着した。今から調さを行う。','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','こちらの様子はつねに私のカメラで見れるはずだし、何かあればずい時連らくするのでよろしく。','sss.jpg','blanc'));
+data.push(new Sean('主人公','はい！','sss.jpg','blanc'));
+data.push(new Sean('副館長','あなたが探偵のブラン氏ですね。お話はうかがっています。ただいま館長がバカンス中のため、私が案内させていただきます。','sss.jpg','fukukan'));
+data.push(new Sean('探偵ブラン','こんにちは、はじめまして。本日はよろしくお願いします。','sss.jpg','blanc,fukukan.png'));
+data.push(new Sean('副館長','こちらこそ。早速当館を案内しましょう。今回ねらわれている美じゅつ品「モナ・リザ」もごしょうかいします。','background','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','おお、これが。','sss.jpg','blanc'));
+data.push(new Sean('副館長','はい、これが "世界で最も有名で、多くの人にかんしょうされ、書かれ、歌にされ、パロディ化された" と言われる有名な絵画「モナ・リザ」です。','sss.jpg','fukukan'));
+data.push(new Sean('副館長','「モナ・リザ」はもともとそこまで有名な作品ではありませんでした。','sss.jpg','fukukan'));
+data.push(new Sean('副館長','しかし、かの有名な画家のピカソもまきこんだとうなん事けんによって、世界から大きく注目され、今では世界で最も有名な絵画とよばれるようになったのです。','sss.jpg','fukukan'));
+data.push(new Sean('副館長','そのかちはげんざい約900億円以上にもなると言われています。','sss.jpg','fukukan'));
+data.push(new Sean('主人公','900億円？！？？！？？？！？','sss.jpg','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','なるほど。芸じゅつ的にも金せん的にもかちがあるということですね。','sss.jpg','blanc,fukukan'));
+data.push(new Sean('副館長','そうなのです。今回の予告状を受けて、当館もセキュリティを強化しました。いくら怪盗Noirでも、かんたんにぬすむことはできないでしょう。','sss.jpg','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','それはそれは！','sss.jpg','blanc,fukukan'));
+data.push(new Sean('全員','？？！','sss.jpg','character'));
+data.push(new Sean('探偵ブラン','みなさん、無事ですか？','sss.jpg','blanc'));
+data.push(new Sean('副館長','私は大じょう夫です。','sss.jpg','fukukan'));
+data.push(new Sean('副館長','ああーーーーっ！！！','sss.jpg','fukukan'));
+data.push(new Sean('探偵ブラン','どうされたのですか？','sss.jpg','blanc'));
+data.push(new Sean('副館長','「モナ・リザ」が、','sss.jpg','fukukan'));
+data.push(new Sean('副館長','失くなっている！','sss.jpg','fukukan'));
+data.push(new Sean('探偵ブラン','なんてことだ！','sss.jpg','blanc'));
+data.push(new Sean('副館長','ああ、どうすればいいんだ......。けい察だ！けい察に連らくしてくれ！早く！','sss.jpg','fukukan'));
+data.push(new Sean('探偵ブラン','もうだめだ、またぬすまれてしまった。','sss.jpg','blanc'));
+data.push(new Sean('主人公','ブランさん？','sss.jpg','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','私は、私はなんて役立たずなんだ！','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','もうあきらめよう、怪盗なんてつかまえられるわけがないんだ。','sss.jpg','blanc'));
+data.push(new Sean('主人公','ブランさん！？！どうして、まだ始まったばかりじゃないですか！あきらめないでください！','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','できるわけない。','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','そもそも世界的な大怪盗に私がいどむ方がおかしかったんだ。','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','申しわけないけど、帰らせてもらおう。もう探偵か業はやめるよ。','sss.jpg','blanc'));
+data.push(new Sean('主人公','(ブランさんの様子がおかしい......どうしたんだろう？)','sss.jpg','blanc'));
+data.push(new Sean('主人公','(ブランさんは何度もあきらめずに怪盗にいどんできたはず......急にあきらめるなんておかしい。)','sss.jpg','blanc'));
+data.push(new Sean('主人公','(ブランさんは何度もあきらめずに怪盗にいどんできたはず......急にあきらめるなんておかしい。)','sss.jpg','blanc'));
+data.push(new Sean('主人公','(ブランさんの様子がおかしい......どうしたんだろう？)','sss.jpg','blanc'));
+data.push(new Sean('主人公','(ブランさんの様子がおかしい......どうしたんだろう？)','sss.jpg','blanc'));
+data.push(new Sean('主人公','(ブランさんの様子がおかしい......どうしたんだろう？)','sss.jpg','blanc'));
+data.push(new Sean('主人公','(ブランさんは何度もあきらめずに怪盗にいどんできたはず......急にあきらめるなんておかしい。)','sss.jpg','blanc'));
+data.push(new Sean('主人公','(！！！)','sss.jpg','blanc'));
+data.push(new Sean('主人公','(まさか！！！)','sss.jpg','blanc'));
+data.push(new Sean('主人公','(この人は探偵ブランではない......？)','sss.jpg','blanc'));
+data.push(new Sean('主人公','あなたは、ブランさんではない！だれだ！','sss.jpg','blanc'));
+data.push(new Sean('？？？','クックック...。ばれてしまったのか。ブランのやつもゆうしゅうな助手をやとったものだ。','sss.jpg','blacknoir'));
+data.push(new Sean('怪盗Noir','やぁ、私は怪盗Noir。君のすい理通りモナリザをぬすみにきたんだ。','sss.jpg','noir'));
+data.push(new Sean('怪盗Noir','私の変そうを見ぬくとはすばらしい。どうだい？私の助手にねがえるというのは？世界各地の美じゅつ品をながめながら楽しむティータイムというのもいいものだよ。','background','noir'));
+data.push(new Sean('主人公','そんなことするわけない！','sss.jpg','noir'));
+data.push(new Sean('？？？','やめろ！私の助手に手を出すな！','sss.jpg','blackblanc'));
+data.push(new Sean('怪盗Noir','おやおや、これはこれは。ブラン君ではないか。君の助手はとてもゆうしゅうだね！おかげで楽しかったよ。','sss.jpg','noir,blanc'));
+data.push(new Sean('探偵ブラン','おい！モナ・リザを返せ！','sss.jpg','noir,blanc'));
+data.push(new Sean('怪盗Noir','それはいただいていくよ。せいぜい私に助手までとられないように気をつけているんだね。','sss.jpg','noir,blanc'));
+data.push(new Sean('怪盗Noir','text','それでは、Adieu！(さようなら！)','noir,blanc'));
+data.push(new Sean('探偵ブラン','よけいなお世話だ！','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','うっ、まぶしいっ','sss.jpg','blanc'));
+data.push(new Sean('主人公','うわあああ！目が！目が！','sss.jpg',''));
+data.push(new Sean('探偵ブラン','にげられてしまったか......','sss.jpg','blanc'));
+data.push(new Sean('主人公','ブランさん！なんとかつかまえる方法はないんですか！？','sss.jpg','blanc'));
+data.push(new Sean('探偵ブラン','うーむ、今Noirがどこにいるのかが分からないときびしいんだ。この美じゅつ館内にいるとは思うのだが......','sss.jpg','blanc'));
+data.push(new Sean('主人公','そんな......','sss.jpg','blanc'));
+data.push(new Sean('主人公','(まてよ......？)','sss.jpg','blanc'));
+data.push(new Sean('主人公','！！！','sss.jpg','blanc'));
+data.push(new Sean('主人公','そうだ！副館長さん、ここの出入り口は1つだけですよね？','sss.jpg','blanc'));
+data.push(new Sean('副館長','そのはずですが。','sss.jpg','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','何か思いついたのか？きみの考えを聞かせてくれ！','sss.jpg','blanc,fukukan'));
+data.push(new Sean('主人公','その出入り口をふさげば、Noirのとうぼうをそ止できると思います！','sss.jpg','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','たしかに......！その方法ならNoirをつかまえることができるかもしれない','sss.jpg','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','いい考えだ','sss.jpg','blanc,fukukan'));
+data.push(new Sean('主人公','副館長さん！今すぐここの出入り口をロックしてください！早く！','sss.jpg','blanc,fukukan'));
+data.push(new Sean('副館長','は、はい！たしか、けいび室のぼうはんシステムを起動すれば出入り口をロックできます。しかし......','sss.jpg','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','しかし......？','sss.jpg','blanc,fukukan'));
+data.push(new Sean('副館長','ぼうはんシステムを起動させるにはパスワードを入力しなければいけません。ですがパスワードを知っているのは館長だけなのです......','background','blanc,fukukan'));
+data.push(new Sean('主人公','なんで副館長なのに知らされてないんですか？！？！','background','blanc,fukukan'));
+data.push(new Sean('副館長','館長はこのようなことをおそれて、自分以外のだれにも知らせていなかったのでしょう。館長の器としては正しいあり方なのでしょうが......','background','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','なるほど......それはこまりましたね','background','blanc,fukukan'));
+data.push(new Sean('副館長','ただ、このようなひじょう事たいにそなえて館長がパスワードのヒントとしていくつかのなぞを用意していました。それがとければぼうはんシステムを起動できます。','background','background','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','それではわたしたちはけいび室にい動する','background','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','きみにも手伝ってもらうことがあるかもしれないがその時はよろしく','background','blanc'));
+data.push(new Sean('主人公','お気をつけて！','background','blanc'));
+data.push(new Sean('副館長','ここがけいび室です。これがぼうはんシステム起動のスイッチのようですね。','background','fukukan'));
+data.push(new Sean('探偵ブラン','8けたの数字を入力する必要があるようですね......４けたずつで分けてとけそうだな、君は前の4けたをといてくれ！わたしは後の4けたをとく！','background','blanc'));
+data.push(new Sean('主人公','ブランさん！パスワードが分かりました！','background','blanc'));
+data.push(new Sean('探偵ブラン','本当かい？私もだ！','background','blanc'));
+data.push(new Sean('主人公','パスワードは「1452」です！','background','blanc'));
+data.push(new Sean('探偵ブラン','ありがとう！私の分と合わせると......','background','blanc'));
+data.push(new Sean('探偵ブラン','「14520415」','background','blanc'));
+data.push(new Sean('探偵ブラン','モナ・リザの作者であるレオナルド・ダ・ヴィンチの生年月日じゃないか！','background','blanc'));
+
+
+
+
+
+
+
+var count = 0;
+
+var skip = function(){
+    document.getElementsByClassName("speakerName")[0].innerHTML=data[count].speakerName;
+    document.getElementsByClassName("speakText")[0].innerHTML=data[count].speakText;
+    document.getElementById("background").src='./img/background/'+ data[count].background;
+    document.getElementById("character1").src='./img/character/'+ data[count].character1+ '.png';
+    count++;
+};
+
+$(function(){
+    $(skip);
+});
+
+$(function () {
+    $(".skipButton").click(function () {
+        $(skip);
+    });
+});
