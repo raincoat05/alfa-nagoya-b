80fa7dc24d0f76dd6b2619795f3a029747cb475a
raincoat05 <kaito_19n1100109@nnn.ed.jp>
2021-09-17 11:33:06 +0900

    iroiro12312

--- js/quiz1.js
+++ js/quiz1.js
@@ -4,9 +4,13 @@ $(function () {
         console.log(value);
         $(function(){
             const answer = value ;
-            if (answer == 'ピエロ'){
+            if (answer == 'ピエロ','ぴえろ'){
                 console.log("正解");
                 $(".hide").css("display","inline");
+                $(".quizText").css("display","none");
+                $(".quizImg").css("display","none");
+                $("#answerContainer").css("display","none");
+                $("#quizContainer").css("height","40vh");
             }else{
                 console.log("不正解");
             };
