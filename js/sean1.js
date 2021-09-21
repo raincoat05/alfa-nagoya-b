98cdb72ee1589290cb036a7a3c66fa34bbf66cf8
raincoat05 <kaito_19n1100109@nnn.ed.jp>
2021-09-21 12:45:18 +0900

    エンディングまでの完成

--- js/sean1.js
+++ js/sean1.js
@@ -9,26 +9,26 @@ class Sean{
 
 var data= [];
 //謎1正解後
-data.push(new Sean('受信音','〜♪','museumOutside.jpg','null'));
-data.push(new Sean('探偵ブラン','おめでとう！！きみが最初の正かい者だ','museumOutside.jpg','blanc'));
-data.push(new Sean('探偵ブラン','ぜひともわたしの助手になってくれ！','museumOutside.jpg','blanc'));
-data.push(new Sean('主人公','もちろんです！','museumOutside.jpg','blanc'));
-data.push(new Sean('探偵ブラン','ありがとう！早速だが説明させてもらおう','museumOutside.jpg','blanc'));
-data.push(new Sean('探偵ブラン','まず、きみにはこの予告状をみてもらいたい','museumOutside.jpg','blanc'));
-data.push(new Sean('探偵ブラン','見ての通りこの予告状は暗号になっている','museumOutside.jpg','blanc'));
-data.push(new Sean('探偵ブラン','この内ようがわからないとわたしたちは怪盗のはん行を止めることができない','museumOutside.jpg','blanc'));
-data.push(new Sean('探偵ブラン','そこで、試験を見事とっぱしたきみに、ぜひこれをといてもらいたい','museumOutside.jpg','blanc'));
-data.push(new Sean('主人公','わかりました！がんばります！','museumOutside.jpg','blanc'));
-data.push(new Sean('主人公','わかりました！がんばります！','museumOutside.jpg','blanc'));
-//謎2に飛ぶ
+data.push(new Sean('受信音','〜♪','bookshelf','null'));
+data.push(new Sean('探偵ブラン','おめでとう！！きみが最初の正かい者だ','bookshelf','blanc'));
+data.push(new Sean('探偵ブラン','ぜひともわたしの助手になってくれ！','bookshelf','blanc'));
+data.push(new Sean('主人公','もちろんです！','bookshelf','blanc'));
+data.push(new Sean('探偵ブラン','ありがとう！早速だが説明させてもらおう','bookshelf','blanc'));
+data.push(new Sean('探偵ブラン','まず、きみにはこの予告状をみてもらいたい','bookshelf','blanc'));
+data.push(new Sean('探偵ブラン','見ての通りこの予告状は暗号になっている','bookshelf','blanc'));
+data.push(new Sean('探偵ブラン','この内ようがわからないとわたしたちは怪盗のはん行を止めることができない','bookshelf','blanc'));
+data.push(new Sean('探偵ブラン','そこで、試験を見事とっぱしたきみに、ぜひこれをといてもらいたい','bookshelf','blanc'));
+data.push(new Sean('主人公','わかりました！がんばります！','bookshelf','blanc'));
+data.push(new Sean('主人公','わかりました！がんばります！','bookshelf','blanc'));
+//謎2
 
 var count = 0;
 
 var skip = function(){
     document.getElementsByClassName("speakerName")[0].innerHTML=data[count].speakerName;
     document.getElementsByClassName("speakText")[0].innerHTML=data[count].speakText;
-    document.getElementById("background").src='./img/background/'+ data[count].background;
-    document.getElementById("character1").src='./img/character/'+ data[count].character1+ '.png';
+    document.getElementById("background").src='./img/background/'+ data[count].background + '.jpg';
+    document.getElementById("character1").src='./img/character/'+ data[count].character1 + '.png';
     count++;
     if(count == 11){
         window.location.href = "/beta-nagoya-b/quiz2.html";
--- js/sean3.js
+++ js/sean3.js
@@ -8,41 +8,28 @@ class Sean{
 };
 
 var data= [];
-data.push(new Sean('主人公','ブランさん！パスワードが分かりました！','background','blanc'));
-data.push(new Sean('探偵ブラン','本当かい？私もだ！','background','blanc'));
-data.push(new Sean('主人公','パスワードは「1452」です！','background','blanc'));
-data.push(new Sean('探偵ブラン','ありがとう！私の分と合わせると......','background','blanc'));
-data.push(new Sean('探偵ブラン','「14520415」','background','blanc'));
-data.push(new Sean('探偵ブラン','「モナ・リザ」の作者であるレオナルド・ダ・ヴィンチの生年月日じゃないか！','background','blanc'));
-data.push(new Sean('主人公','館長はどれだけ「モナ・リザ」が好きなんだ......','background','blanc,fukukan'));
-data.push(new Sean('副館長','それでは、ぼうはんシステムを起動します。','background','fukukan'));
-data.push(new Sean('副館長','......よし！出入り口のふうさに成功しました。','background','fukukan'));
-data.push(new Sean('探偵ブラン','副館長、ありがとうございます。君もありがとう、たよりになるな。','background','blanc'));
-data.push(new Sean('主人公','光栄です。さあ、Noirは必ずこの付近に必ずいるはずです。さがしましょう！','background','blanc,fukukan'));
-data.push(new Sean('探偵ブラン','この付近にNoirはいるはずだが......','background','blanc,fukukan'));
-data.push(new Sean('副館長','見当たりませんね。','background','blanc,fukukan'));
-data.push(new Sean('主人公','ブランさん、そこに何か落ちてませんか？','background','blanc,fukukan'));
-data.push(new Sean('探偵ブラン','お、なんだこれは？','background','blanc,fukukan'));
-data.push(new Sean('探偵ブラン','くっ、また　Noirをのがしたか......','background','blanc,fukukan'));
-data.push(new Sean('主人公','おしいところまでは行ったみたいですけどね','background','blanc,fukukan'));
-data.push(new Sean('探偵ブラン','次こそはつかまえてやる！ところで、今回ここまで追いつめることができたのもきみのおかげだ　感しゃしている','background','blanc'));
-data.push(new Sean('主人公','こちらこそです、ブランさん。それではモナ・リザが無事か確認に行きましょう！','background','blanc'));
-data.push(new Sean('探偵ブラン','そうしよう！','background','blanc'));
-data.push(new Sean('探偵ブラン','そうしよう！これにて事件解決だ！','background','blanc'));
-//エンディング
-data.push(new Sean('主人公','ところでブランさん、入れかわっていた時を思い返すと、Noirとやけに顔にてませんか......？','background','blanc'));
-data.push(new Sean('探偵ブラン','ああそれは、ふたごなんだよ','background','blanc'));
-data.push(new Sean('主人公','え？','background','blanc'));
-data.push(new Sean('探偵ブラン','おっと、いらい人によばれてしまった　少し席を外すよ　それでは失礼','background','blanc'));
-data.push(new Sean('主人公','ちょっとブランさん？！','background','null'));
-//家にて
-data.push(new Sean('主人公','ふぅ......','background','null'));
-data.push(new Sean('主人公','すごく大変だったなぁー。まさかブランさんと怪盗Noirがふたごだったなんて。','background','null'));
-data.push(new Sean('主人公','......どっちが姉なんだ？','background','null'));
-data.push(new Sean('メールの着信音','〜♪','background','null'));
-data.push(new Sean('主人公','？？？','background','null'));
-data.push(new Sean('主人公','Noir......Noir！？','background','null'));
-data.push(new Sean('主人公','Noir......Noir！？','background','null'));
+data.push(new Sean('主人公','ブランさん！パスワードが分かりました！','securityRoom','blanc'));
+data.push(new Sean('探偵ブラン','本当かい？私もだ！','securityRoom','blanc'));
+data.push(new Sean('主人公','パスワードは「1452」です！','securityRoom','blanc'));
+data.push(new Sean('探偵ブラン','ありがとう！私の分と合わせると......','securityRoom','blanc'));
+data.push(new Sean('探偵ブラン','「14520415」','securityRoom','blanc'));
+data.push(new Sean('探偵ブラン','「モナ・リザ」の作者であるレオナルド・ダ・ヴィンチの生年月日じゃないか！','securityRoom','blanc'));
+data.push(new Sean('主人公','館長はどれだけ「モナ・リザ」が好きなんだ......','securityRoom','blanc,fukukan'));
+data.push(new Sean('副館長','それでは、ぼうはんシステムを起動します。','securityRoom','fukukan'));
+data.push(new Sean('副館長','......よし！出入り口のふうさに成功しました。','securityRoom','fukukan'));
+data.push(new Sean('探偵ブラン','副館長、ありがとうございます。君もありがとう、たよりになるな。','securityRoom','blanc'));
+data.push(new Sean('主人公','光栄です。さあ、Noirは必ずこの付近に必ずいるはずです。さがしましょう！','securityRoom','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','この付近にNoirはいるはずだが......','artmuseumInside','blanc,fukukan'));
+data.push(new Sean('副館長','見当たりませんね。','artmuseumInside','blanc,fukukan'));
+data.push(new Sean('主人公','ブランさん、そこに何か落ちてませんか？','artmuseumInside','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','お、なんだこれは？','artmuseumInside','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','くっ、また　Noirをのがしたか......','artmuseumInside','blanc,fukukan'));
+data.push(new Sean('主人公','おしいところまでは行ったみたいですけどね','artmuseumInside','blanc,fukukan'));
+data.push(new Sean('探偵ブラン','次こそはつかまえてやる！ところで、今回ここまで追いつめることができたのもきみのおかげだ　感しゃしている','artmuseumInside','blanc'));
+data.push(new Sean('主人公','こちらこそです、ブランさん。それではモナ・リザが無事か確認に行きましょう！','artmuseumInside','blanc'));
+data.push(new Sean('探偵ブラン','そうしよう！','artmuseumInside','blanc'));
+data.push(new Sean('null','〜END〜','monalisaUp','null'));
+data.push(new Sean('null','〜END〜','monalisaUp','null'));
 //メールの画像表示、真のエンディング？
 
 var count = 0;
@@ -50,10 +37,10 @@ var count = 0;
 var skip = function(){
     document.getElementsByClassName("speakerName")[0].innerHTML=data[count].speakerName;
     document.getElementsByClassName("speakText")[0].innerHTML=data[count].speakText;
-    document.getElementById("background").src='./img/background/'+ data[count].background;
+    document.getElementById("background").src='./img/background/'+ data[count].background + '.jpg';
     document.getElementById("character1").src='./img/character/'+ data[count].character1+ '.png';
     count++;
-    if(count == 31){
+    if(count == 21){
         window.location.href = "/beta-nagoya-b/ending.html";
     };
 };
