50209fbb137efe40118f4fa9657adad9c1d53aec
raincoat05 <kaito_19n1100109@nnn.ed.jp>
2021-09-22 22:49:10 +0900

    色々

--- js/sean1.js
+++ js/sean1.js
@@ -15,6 +15,7 @@ data.push(new Sean('探偵ブラン','ぜひともわたしの助手になって
 data.push(new Sean('主人公','もちろんです！','bookshelf','blanc'));
 data.push(new Sean('探偵ブラン','ありがとう！早速だが説明させてもらおう','bookshelf','blanc'));
 data.push(new Sean('探偵ブラン','まず、きみにはこの予告状をみてもらいたい','bookshelf','blanc'));
+data.push(new Sean('探偵ブラン','まず、きみにはこの予告状をみてもらいたい','preview','blanc'));
 data.push(new Sean('探偵ブラン','見ての通りこの予告状は暗号になっている','bookshelf','blanc'));
 data.push(new Sean('探偵ブラン','この内ようがわからないとわたしたちは怪盗のはん行を止めることができない','bookshelf','blanc'));
 data.push(new Sean('探偵ブラン','そこで、試験を見事とっぱしたきみに、ぜひこれをといてもらいたい','bookshelf','blanc'));
@@ -34,7 +35,16 @@ var skip = function(){
         var media = document.getElementById("audio");
         media.volume = 0.12
     }
-    if(count == 11){
+    if(count == 7){
+        $("#textBox").css("display","none");
+        $("#background").css("filter", "none");
+        $("#characterBox").css("display","none");
+    }else{
+        $("#textBox").css("display","block");
+        $("#background").css("filter", "blur(2px) grayscale(90%)");
+        $("#characterBox").css("display","block");
+    }
+    if(count == 12){
         window.location.href = "/beta-nagoya-b/quiz2.html";
     };
 };
@@ -47,6 +57,14 @@ $(function(){
 $(function () {
     $(".skipButton").click(function () {
         $(skip);
+    });
+});
+
+$(function () {
+    $(".exskip").click(function () {
+        if (count == 7) {
+            $(skip);
+        }
     });
 });
 
